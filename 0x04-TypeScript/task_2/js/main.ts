bd8c9c4d034de4f335c1564208bfea14d239f7dd
Adedayoke <adedayoke2006@gmail.com>
2025-09-21 19:01:04 +0100

    Main task 2 push

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,7 +39,8 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
+  salary = typeof salary === 'number' ? salary : parseInt(salary);
+  if (salary < 500) {
     return new Teacher();
   } else {
     return new Director();

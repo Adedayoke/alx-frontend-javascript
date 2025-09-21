18214b3b47467f121e93df79f1a9d3a2aafd8bf3
Adedayoke <adedayoke2006@gmail.com>
2025-09-21 18:58:29 +0100

    Task 3 fix

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -50,7 +50,7 @@ interface StudentClassInterface {
   displayName(): string;
 }
 
-class StudentClass implements StudentClassInterface {
+class StudentClass {
   firstName: string;
   lastName: string;
 
